7274d31ad3bfc2c793e7e513788ea844aaad62b6
pulkittaneja09 <pulkittaneja0509@gmail.com>
2026-06-27 20:19:09 +0530

    updated main.py code

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
+
 from app.api.upload import router as upload_router
+from app.api.query import router as query_router
 
 app = FastAPI(title="Enterprise Knowledge Assistant")
 
 app.include_router(upload_router)
+app.include_router(query_router)
 
 
 @app.get("/")
