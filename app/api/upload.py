6c9ff0b30b854f1da8cba1d281aeef55e727e2fd
pulkittaneja09 <pulkittaneja0509@gmail.com>
2026-06-27 18:34:09 +0530

    update upload code with chunking logic

--- app/api/upload.py
+++ app/api/upload.py
@@ -1,5 +1,5 @@
 import os
-
+from app.services.text_splitter import split_documents
 from fastapi import APIRouter, UploadFile, File, HTTPException
 from app.services.pdf_loader import load_pdf
 router = APIRouter()
@@ -20,13 +20,22 @@ async def upload_pdf(file: UploadFile = File(...)):
         buffer.write(await file.read())
 
     documents = load_pdf(file_path)
+    chunks = split_documents(documents)
 
+    print(f"Total Chunks : {len(chunks)}")
+
+    print("\nFirst Chunk:\n")
+    print(chunks[0].page_content)
+
+    print("\nMetadata:")
+    print(chunks[0].metadata)
     print(f"Pages Loaded: {len(documents)}")
     print(documents[0].page_content[:500])
 
     return {
-        "success": True,
-        "filename": file.filename,
-        "pages": len(documents),
-        "message": "PDF uploaded and loaded successfully."
-    }
+    "success": True,
+    "filename": file.filename,
+    "pages": len(documents),
+    "chunks": len(chunks),
+    "message": "PDF uploaded and processed successfully."
+}
