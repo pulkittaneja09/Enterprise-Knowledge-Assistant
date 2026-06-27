ed9cee3975bbeb10d6d17797d8a989df7001ae68
pulkittaneja09 <pulkittaneja0509@gmail.com>
2026-06-27 16:57:08 +0530

    updated upload code

--- app/api/upload.py
+++ app/api/upload.py
@@ -1,30 +1,32 @@
 import os
 
 from fastapi import APIRouter, UploadFile, File, HTTPException
-
+from app.services.pdf_loader import load_pdf
 router = APIRouter()
 
 UPLOAD_DIR = "uploads"
 
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
-
 @router.post("/upload")
 async def upload_pdf(file: UploadFile = File(...)):
 
     if file.content_type != "application/pdf":
-        raise HTTPException(
-            status_code=400,
-            detail="Only PDF files are allowed."
-        )
+        raise HTTPException(status_code=400, detail="Only PDF files are allowed.")
 
     file_path = os.path.join(UPLOAD_DIR, file.filename)
 
     with open(file_path, "wb") as buffer:
         buffer.write(await file.read())
 
+    documents = load_pdf(file_path)
+
+    print(f"Pages Loaded: {len(documents)}")
+    print(documents[0].page_content[:500])
+
     return {
         "success": True,
         "filename": file.filename,
-        "message": "PDF uploaded successfully."
+        "pages": len(documents),
+        "message": "PDF uploaded and loaded successfully."
     }
